1f5e7f7eb887c68e5c1e8f3153e7534d03d380f8
Hakob <hakobhakobyan389@gmail.com>
2023-05-09 02:12:54 +0400

    Initial commit

---
+++ src/main/java/com/example/listtt/ListttApplication.java
@@ -0,0 +1,13 @@
+package com.example.listtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ListttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/listtt/ListttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.listtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
